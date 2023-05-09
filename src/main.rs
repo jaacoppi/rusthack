8dd88f8347c301117218638a72166653a741dc5c
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-09 21:28:56 +0300

    Diceroller: functions, crates.io dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use diceroll::*;
+extern crate diceroll;
+
 fn main() {
     let item = Item {kind: ItemType::TOOL, name: String::from("lockpick"), uses: -1};
     let user = Creature {name: String::from("Juho"), symbol: '@', hp: 100, item: item};
 
     println!("Hello, world! {0}, {1}, {2} with a {3} with {4} uses", user.name, user.symbol, user.hp, user.item.name, user.item.uses);
+    diceroller(2,10);
 }
 
 
@@ -22,4 +26,15 @@ struct Item {
 enum ItemType {
     TOOL,
     WEAPON
+}
+
+
+
+fn diceroller(dice: i32, sides: i32) {
+    let modifier = 2;
+    let result = roll(DiceRoll::new()
+        .dice(dice)
+        .sides(sides)
+        .modifier(modifier));
+    println!("We rolled {}d{}+{}, which yielded a total of {}.", dice, sides, modifier, result.total);
 }
