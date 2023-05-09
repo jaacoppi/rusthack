53cb1c2e97ca0a21d981a291383d5112ec1d8273
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-09 20:40:06 +0300

    Struct inside a struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 fn main() {
-    let user = Creature {name: String::from("Juho"), symbol: '@', hp: 100};
+    let item = Item {name: String::from("lockpick"), uses: -1, itemtype: String::from("tool")};
+    let user = Creature {name: String::from("Juho"), symbol: '@', hp: 100, item: item};
 
-    println!("Hello, world! {0}, {1}, {2}", user.name, user.symbol, user.hp);
+    println!("Hello, world! {0}, {1}, {2} with a {3} {4} with {5} uses", user.name, user.symbol, user.hp, user.item.itemtype, user.item.name, user.item.uses);
 }
 
 
 struct Creature {
     name: String,
     symbol: char,
-    hp: u32
+    hp: i32,
+    item: Item
+}
+
+struct Item {
+    name: String,
+    itemtype: String,
+    uses: i32
 }
