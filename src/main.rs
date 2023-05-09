d915dd007648d83aee7f44a14eb488283ab48f55
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-09 20:26:52 +0300

    first struct!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let user = Creature {name: String::from("Juho"), symbol: '@', hp: 100};
+
+    println!("Hello, world! {0}, {1}, {2}", user.name, user.symbol, user.hp);
+}
+
+
+struct Creature {
+    name: String,
+    symbol: char,
+    hp: u32
 }
