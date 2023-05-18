f117c5ad858ee6bb8c850486eb0af72e3d93598b
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-18 21:33:03 +0300

    mutables and & references -> pointers?

--- src/creatures.rs
+++ src/creatures.rs
@@ -1,16 +1,25 @@
 use dice::throw;
 
 
-pub fn attack(attacker: &Creature, defender: &Creature) {
+pub fn attack(attacker: &mut Creature, defender: &mut Creature) {
     let damage = throw(&attacker.item.damage);
-    println!("{}", damage);
+    do_damage(defender, damage);
+    println!("{}", defender.hp);
 // defender.hp - throw
 // status - dead?
 }
 
+fn do_damage(target: &mut Creature, amount: i32) {
+    target.hp -= amount;
+
+    if target.hp < 0 {
+        println!("Dead!");
+    }
+}
+
 pub fn random_creature() -> Creature {
     let item = Item {kind: ItemType::TOOL, name: String::from("lockpick"), uses: -1, damage: String::from("2d4+1")};
-    let creature = Creature {name: String::from("Juho"), symbol: '@', hp: 100, item: item};
+    let creature = Creature {name: String::from("Juho"), symbol: '@', hp: 5, item: item};
     return creature;
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ extern crate diceroll;
 use creatures::*;
 
 fn main() {
-
-
-    let user = random_creature();
-    let enemy = random_creature();
-    creatures::attack(&user, &enemy);
+    let mut user = random_creature();
+    let mut enemy = random_creature();
+    creatures::attack(&mut user, &mut enemy);
 
     println!("Hello, world! {0}, {1}, {2} with a {3} with {4} uses", user.name, user.symbol, user.hp, user.item.name, user.item.uses);
     dice::throw(&user.item.damage);
