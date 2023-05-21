077d33e42e324dfc221f331f98248b924fc6b854
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-21 17:13:08 +0300

    initiative. Note that we're passing too big privileges - just an integer should be enough.

--- src/creatures.rs
+++ src/creatures.rs
@@ -2,6 +2,7 @@ use dice::throw;
 
 
 pub fn attack(attacker: &mut Creature, defender: &mut Creature) {
+    decide_initiative(attacker, defender);
     let damage = throw(&attacker.item.damage);
     do_damage(defender, damage);
     println!("{}", defender.hp);
@@ -9,6 +10,19 @@ pub fn attack(attacker: &mut Creature, defender: &mut Creature) {
 // status - dead?
 }
 
+fn decide_initiative(attacker: &mut Creature, defender: &mut Creature) -> bool {
+    let attacker_ini = throw(&attacker.initiative);
+    let defender_ini = throw(&defender.initiative);
+
+    if attacker_ini >= defender_ini {
+        println!("Attacker goes first!");
+        return true;
+    } else {
+        println!("Defender goes first!");
+        return false;
+
+    }
+}
 fn do_damage(target: &mut Creature, amount: i32) {
     target.hp -= amount;
 
@@ -19,7 +33,7 @@ fn do_damage(target: &mut Creature, amount: i32) {
 
 pub fn random_creature() -> Creature {
     let item = Item {kind: ItemType::TOOL, name: String::from("lockpick"), uses: -1, damage: String::from("2d4+1")};
-    let creature = Creature {name: String::from("Juho"), symbol: '@', hp: 5, item: item};
+    let creature = Creature {name: String::from("Juho"), symbol: '@', hp: 5, item: item, initiative: String::from("2d4+0")};
     return creature;
 }
 
@@ -28,7 +42,8 @@ pub struct Creature {
     pub name: String,
     pub symbol: char,
     pub hp: i32,
-    pub item: Item
+    pub item: Item,
+    pub initiative: String
 }
 
 pub struct Item {
