73fb49966a2e3bb77461075a552bd624399cc422
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-27 11:49:28 +0300

    reading keyboard input

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@ pub mod creatures;
 use creatures::*;
 
 fn main() {
-    let mut user = Creature::new_random(String::from("Juho"));
+    println!("What do they call you, warrior?");
+
+    let name = read_name();
+
+    let mut user = Creature::new_random(name);
     println!("Hello, world! {0}, {1}, {2} with a {3} with {4} uses", user.name, user.symbol, user.hp, user.item.name, user.item.uses);
     let mut enemy = Creature::new_random(String::from("Pahis"));
     println!("Enemy:! {0}, {1}, {2} with a {3} with {4} uses", enemy.name, user.symbol, enemy.hp, enemy.item.name, enemy.item.uses);
     user.attack(&mut enemy);
 
+}
+
+fn read_name() -> String {
+    let mut name = String::new();
+    let _input = std::io::stdin().read_line(&mut name).expect("Failed to read line");
+    name = name.replace("\n", "");
+
+    name
 }
