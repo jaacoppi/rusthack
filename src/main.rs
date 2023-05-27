23a8f9b1437c6efd73eb65e2c691bba512d2eec6
Juho Hiltunen <jaacoppi@kapsi.fi>
2023-05-27 19:13:28 +0300

    handling Result and loop

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ pub mod creatures;
 use creatures::*;
 
 fn main() {
-    println!("What do they call you, warrior?");
 
-    let name = read_name();
+    let name = loop {
+        println!("What do they call you, warrior?");
+        let input = read_input(5);
+        if input.is_err() {
+            println!("{}: That name is too long for a warrior!", input.unwrap_err());
+            continue;
+        }
+        break input.unwrap();
+    };
 
     let mut user = Creature::new_random(name);
     println!("Hello, world! {0}, {1}, {2} with a {3} with {4} uses", user.name, user.symbol, user.hp, user.item.name, user.item.uses);
@@ -16,10 +23,14 @@ fn main() {
 
 }
 
-fn read_name() -> String {
-    let mut name = String::new();
-    let _input = std::io::stdin().read_line(&mut name).expect("Failed to read line");
-    name = name.replace("\n", "");
+fn read_input(max_length: usize) -> Result<String, &'static str> {
+    let mut input = String::new();
+    let len = std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    input = input.replace("\n", "");
 
-    name
+    if len -1 <= max_length {
+        Ok(input)
+    } else {
+        Err("Input too long!")
+    }
 }
